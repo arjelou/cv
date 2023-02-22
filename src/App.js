3cc22dd0bb6574173dbdfd4968908532b119f6f0
Jelou <arjelou.jelou@gmail.com>
2023-02-22 16:52:26 +0800

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './global.css';
+import Navbar from './component/navbar';
+import { BsGithub,BsArrowRight,BsPersonCircle } from "react-icons/bs";
 
-function App() {
+
+export default class App extends React.Component {
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+    <div className='container'>
+      <div>
+        <section className=''>
+         <Navbar />
+        </section>
+      </div>
+      <div>
+        <section className='content'>
+          <BsPersonCircle size={170} color='blue'/>
+          <h2>Arjelou Carampatana</h2>
+        <p>Web Developer</p>
+        <button>Connect <BsArrowRight size={20} /></button><br />
+        <BsGithub />
+        </section>
+      </div>
+      <div>
+      <div className='gap-3 d-flex justify-content-start'>
+        <p>Contact</p>
+         <p>About</p>
+         <p>Skills</p>
+       </div>
+      </div>
     </div>
-  );
+  </>
+  )
 }
-
-export default App;
+}
