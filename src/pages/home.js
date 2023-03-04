1a2286694df3ce33136823486b31c812541f7a17
Jelou <arjelou.jelou@gmail.com>
2023-03-04 18:31:51 +0800

    update ui

--- src/pages/home.js
+++ src/pages/home.js
@@ -15,19 +15,14 @@ render() {
       <article>
         <img src={avatar} alt='im_avatar' />
         <h2>Arjelou Carampatana</h2>
-        <p>Web Developer</p>
-      </article>
-    </section>
-    <section className='bio'>
-      <article>
-          <p>
+        <h6>Web Developer</h6>
+        <p>
           As a career shifter from IT Support to web development, I am determined to develop my skills
-          further and use my technical knowledge to create innovative web applications. I am excited to
-          work with the tech stack of MySQL, Express.js, React.js, and Node.js and use Redux for state
-          management. My goal is to be a valuable development team member and continuously learn and
+          further and use my technical knowledge to create innovative web applications. My goal is to be
+          a valuable development team member and continuously learn and
           grow as a web developer.
           </p>
-        </article>
+      </article>
     </section>
     <section className='cv_tools'>
       <h3>Skills</h3>
@@ -37,33 +32,50 @@ render() {
         <NavLink className={({isActive}) => isActive ? "cv__active" : "cv_inactive"} to='/tools'>Tools</NavLink>
       </article>
     </section>
-    <div>
+    <section className='outlet'>
         <Outlet />
-    </div>
+    </section>
     <section className='cv__projets'>
       <h3>Projects</h3>
-      <article>
+      <article className='project1'>
         <h4>B2B eCommerce</h4>
         <p>Web app built with React,
           Node, Express, and MySQL for
           the manufacturing of
           garments that serve other
           companies' needs.
-          <NavLink to='https://github.com/arjelou/ecommerce'><BsGithub title='Github' className='icon'/> </NavLink>
+          {/* <NavLink to='https://github.com/arjelou/ecommerce'><BsGithub title='Github' className='icon'/> </NavLink> */}
           <NavLink to='https://ecommerce-arjelou.vercel.app/'><BsBoxArrowUpRight title='Link' className='icon'/> </NavLink>
-
         </p>
+        <NavLink to='https://github.com/arjelou/ecommercebackend'><BsGithub title='Github' className='icon'/> </NavLink>
+
       </article>
       <br />
-      <article>
+      <article className='project2'>
         <h4>B2B eCommerce Back End</h4>
         <p>Web API built with
           Node, Express, and ORM/MySQL for
           the manufacturing of
           garments that serve other
           companies' needs.
-          <NavLink to='https://github.com/arjelou/ecommercebackend'><BsGithub title='Github' className='icon'/> </NavLink>
+          <NavLink to='https://ecommerce-arjelou.vercel.app/'><BsBoxArrowUpRight title='Link' className='icon'/> </NavLink>    
         </p>
+        <NavLink to='https://github.com/arjelou/ecommercebackend'><BsGithub title='Github' className='icon'/> </NavLink>
+      </article>
+    </section>
+    <section className='cv__contact'>
+      <h3>GET IN TOUCH</h3>
+      <article className='__email'>
+        <h5>EMAIL</h5>
+        <h6>ARJELOU.JELOU@GMAIL.COM</h6>
+      </article>
+      <article className='__phone'>
+        <h5>PHONE</h5>
+        <h6>+63976-395-9021</h6>
+      </article>
+      <article className='__address'>
+        <h5>ADDRESS</h5>
+        <h6>CALINAN, DAVAO CITY 8000</h6>
       </article>
     </section>
   </div>
