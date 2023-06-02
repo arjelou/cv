fd92129914431f4d9952d85a6b6a9764bfe3911c
Jelou <arjelou.jelou@gmail.com>
2023-06-03 04:54:28 +0800

    fix emailJS config

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -15,35 +15,29 @@ export const ContactUs = () => {
         theme: "colored",
     });
     const form = useRef();
-
     const sendEmail = (e) => {
-      if (e.target.user_name === "" || e.target.user_email === "" || e.target.message === "") {
-        alert("Please enter all");
-      }else{
-        e.preventDefault();
-        emailjs.sendForm('service_c0gety8', 'template_v34jpe4', form.current, 'Ki_-V7Bo-7_1TT7LF')
-          .then((result) => {
-              console.log(result.text); 
-              notify()
-          }, (error) => {
-              console.log(error.text);
-          });
+      e.preventDefault();
+      emailjs.sendForm('service_c0gety8', 'template_v34jpe4', form.current, 'Ki_-V7Bo-7_1TT7LF')
+        .then((result) => {
+            console.log(result.text); 
+            notify()
+        }, (error) => {
+            console.log(error.text);
+        });
       };
-    }
-
   return (
     <div className='contactUS'>
     <p>arjelou.jelou@gmail.com</p>
     <ToastContainer />
     <form ref={form} onSubmit={sendEmail}>
       <label>Name</label>
-      <input type="text" name="user_name" onChange={(e) => sendEmail()}/>
+      <input type="text" name="user_name" onChange={(e) => sendEmail()} required/>
       <label>Email</label>
-      <input type="email" name="user_email" />
+      <input type="email" name="user_email" required/>
       <label>Message</label>
       <textarea name="message" />
-      <input className='btn_submit' type="submit" value="Send" />
+      <input className='btn_submit' type="submit" value="Send" required />
     </form>
     </div>
   );
-};
+}
